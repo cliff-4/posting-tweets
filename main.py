b2469c6f8a22e2c23f050ed517c38839ad418c90
cliff-4 <adityamishra0700@gmail.com>
2022-06-12 20:32:24 +0530

    Added authenticating to name=main itself added 4 new functions

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import tweepy # installed tweepy
 import json
+import time
+
+with open('credentials.json', "r") as j: creds = json.load(j)
+with open("personal/tweet_ids.json", "r") as t: ids = json.load(t)
 
 def OAuth():
-	with open('credentials.json', "r") as j: creds = json.load(j)
 	try:
 		auth = tweepy.OAuthHandler(creds["api_key"], creds["api_key_secret"])
 		auth.set_access_token(creds["access_token"], creds["access_token_secret"])
@@ -11,7 +14,6 @@ def OAuth():
 		return None
 
 def tweet_text():
-	api = tweepy.API(OAuth())
 
 	text = "suffering"
 	#with open('personal/i.txt', "r") as file: text = file.read()
@@ -43,7 +45,6 @@ def word_thread(word, tweet_id):
 	Then posts a tweet thread with individual letters as thread components.
 	Note that two consecutive letters can't be the same.
 	"""
-	api = tweepy.API(OAuth())
 
 	prev_id = tweet_id
 	for i in word:
@@ -54,9 +55,45 @@ def word_thread(word, tweet_id):
 		)
 		prev_id = id1.id
 
+def create_and_delete():
+	"""Creates a tweet and deletes it 60 seconds later."""
+	id = api.update_status("Hello there! This will be delete in 60 seconds")
+	for i in range(60):
+		print("T-", 60-i, " seconds.", end="\r")
+		time.sleep(1)
+	api.destroy_status(id.id)
+
+def posting_file(ids):
+	api.update_status(
+		status = "https://github.com/cliff-4/posting-tweets",
+		in_reply_to_status_id = ids["Hello World"]
+	)
+
+def make_text_commentable(text):
+	user_id = ids["self_id"]
+	username = api.get_user(user_id = ids["self_id"]).screen_name
+	
+	return ("@" + username + " " + text)
+
+def media_tweet():
+	in_reply_to_id = ids["Hello World"]
+
+	status_text = "GIF!"
+	file_address = "personal/kpop dance.gif"
+
+	api.update_status_with_media(
+		status = status_text,
+		filename = file_address,
+		in_reply_to_status_id = in_reply_to_id
+	)
+
 if __name__ == "__main__":
-	with open("personal/tweet_ids.json", "r") as t: ids = json.load(t)
-	#text = input()
+	api = tweepy.API(OAuth()) #authenticating. always keep this here.
+	
 	#tweet_text()
-	word_thread("STRING", ids["dump_thread"])
+	#word_thread("STRING", ids["dump_thread"])
+	#create_and_delete()
+	#posting_file(ids)
+	#media_tweet()
+	
 	print("Success")
